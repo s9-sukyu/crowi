dc35b586bdf6ff0aa1a3886c6b533e9c61e0913b
sapphi-red <green@sapphi.red>
2020-01-30 23:54:39 +0900

    limit default shown seen user number

--- client/components/SeenUserList/UserList.js
+++ client/components/SeenUserList/UserList.js
@@ -3,7 +3,18 @@ import PropTypes from 'prop-types'
 
 import UserPicture from 'components/User/UserPicture'
 
+import { Button } from 'react-bootstrap'
+
+const DEFAULT_SHOWN_USERS = 30
+
 export default class UserList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isShownAll: false,
+    }
+  }
+
   isSeenUserListShown() {
     const userCount = this.props.users.length
     if (userCount > 0 && userCount <= 114514) {
@@ -18,7 +29,18 @@ export default class UserList extends React.Component {
       return null
     }
 
-    const users = this.props.users.map(user => {
+    let isShownAll = this.state.isShownAll
+    const userLen = this.props.users.length
+    if (userLen < DEFAULT_SHOWN_USERS) {
+      this.setState({
+        isShownAll: true,
+      })
+      isShownAll = true
+    }
+
+    const usersData = isShownAll ? this.props.users : this.props.users.slice(0, DEFAULT_SHOWN_USERS)
+
+    const users = usersData.map(user => {
       return (
         <a key={user._id} data-user-id={user._id} href={'/user/' + user.username} title={user.name}>
           <UserPicture user={user} size="xs" />
@@ -26,7 +48,18 @@ export default class UserList extends React.Component {
       )
     })
 
-    return <p className="seen-user-list">{users}</p>
+    return (
+      <p className="seen-user-list">
+        {users}
+        {!isShownAll ? (
+          <Button className="seen-user-show-more-button" style={{ display: 'block' }} onClick={() => this.setState({ isShownAll: true })}>
+            もっと表示する
+          </Button>
+        ) : (
+          ''
+        )}
+      </p>
+    )
   }
 }
 
