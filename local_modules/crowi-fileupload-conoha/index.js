1a34c8eff363b3b681e1d52216426815b58630d9
mazrean <mazrean@localhost.localdomain>
2020-04-14 14:17:43 +0900

    ログ出力停止

--- local_modules/crowi-fileupload-conoha/index.js
+++ local_modules/crowi-fileupload-conoha/index.js
@@ -24,41 +24,29 @@ module.exports = crowi => {
 
   return {
     uploadFile: async (remote, contentType, fileStream, options) => {
-      console.warn('File Upload:Start Make Stream')
       const uploadStream = swift.upload({ container: config.container, contentType, remote })
-      console.warn(`File Upload:End Make Stream`)
 
       return pipeline(fileStream, uploadStream)
     },
     findDeliveryFile: async (fileId, remote) => {
-      console.warn('File Find:Start Make File Name')
       const local = lib.createCacheFileName(fileId)
       if (!lib.shouldUpdateCacheFile(local)) {
         return local
       }
-      console.warn('File Find:End Make File Name')
 
-      console.warn('File Find:Start Make Stream')
       const downloadStream = swift.download({ container: config.container, remote })
-      console.warn('File Find:End Make Stream')
       await pipeline(downloadStream, fs.createWriteStream(local))
       return local
     },
     deleteFile: (fileId, remote) =>
       new Promise((resolve, reject) => {
-        console.warn('File Remove:Start')
         swift.removeFile(config.container, remote, (err, result) => {
           if (err) {
             return reject(err)
           }
-          console.warn('File Remove:Start Resolve')
           resolve(result)
-          console.warn('File Remove:End Resolve')
-          console.warn('File Remove:Start Clear Cache')
           lib.clearCache(fileId)
-          console.warn('File Remove:End Clear Cache')
         })
-        console.warn('File Remove:End')
       }),
     generateUrl: remote => `https://object-storage.${config.region}.conoha.io/v1/nc_${config.tenantId}/${config.container}/${remote}`,
   }
